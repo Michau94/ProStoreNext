d2ab43d335e5140bc3eb0ae720815ec4e71b343f
MichalWadowski <80638034+Michau94@users.noreply.github.com>
2026-01-21 18:02:43 +0100

    not-found and loading page

---
+++ prostore/app/not-found.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="/images/logo.svg"
+        alt={`${APP_NAME} logo`}
+        width={48}
+        height={48}
+        priority={true}
+      />
+      <div className="p-6 rounded-lg w-1/3 shadow-md text-center">
+        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+        <p className="text-destructive">Could not find the requested page.</p>
+        <Button
+          variant="outline"
+          className="mt-4 ml-2"
+          onClick={() => (window.location.href = "/")}
+        >
+          Back to Home
+        </Button>
+      </div>
+    </div>
+  );
+}
