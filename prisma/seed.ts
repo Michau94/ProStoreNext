718c7653086d0278f558e87142483764dd420cf2
MichalWadowski <80638034+Michau94@users.noreply.github.com>
2026-01-27 15:26:17 +0100

    Update prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "../lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../lib/generated/prisma";
+
 import "dotenv/config";
 import sampleData from "../db/sample-data";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 async function main() {
   const adapter = new PrismaPg({
@@ -14,14 +15,27 @@ async function main() {
     console.log("Starting database seed...");
 
     await prisma.product.deleteMany({});
+    await prisma.account.deleteMany({});
+    await prisma.session.deleteMany({});
+    await prisma.verificationToken.deleteMany({});
+    await prisma.user.deleteMany({});
+
     console.log("Cleared existing products");
 
     await prisma.product.createMany({
       data: sampleData.products,
     });
 
+    await prisma.user.createMany({
+      data: sampleData.users,
+    });
+
     console.log(
       `Database has been seeded with ${sampleData.products.length} products successfully.`,
+    );
+
+    console.log(
+      `Database has been seeded with ${sampleData.users.length} users successfully.`,
     );
   } catch (error) {
     console.error("Error during seeding:", error);
