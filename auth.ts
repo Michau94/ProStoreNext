73c18bea225e267389f711007a1e56396cbf9064
MichalWadowski <80638034+Michau94@users.noreply.github.com>
2026-01-28 11:16:40 +0100

    Add Auth Route

---
+++ auth.ts
@@ -0,0 +1,78 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/db/prisma";
+import { compareSync } from "bcrypt-ts-edge";
+import type { NextAuthOptions } from "next-auth";
+
+export const config: NextAuthOptions = {
+  pages: {
+    signIn: "/signin",
+    error: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, //30 days
+  },
+  // Remove adapter when using jwt strategy with credentials provider
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        // Find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        // Check if user exists and if the password matches
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password,
+          );
+
+          // If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        // If user does not exist or password does not match return null
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, trigger }: any) {
+      // set user id from token
+      session.user.id = token.sub;
+
+      // if update set user name
+      if (trigger === "update") {
+        session.user.name = token.name;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
