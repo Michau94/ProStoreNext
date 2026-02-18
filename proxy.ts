b92e12598e4a6f0b51d7c8b21ef4541487ca3d65
MichalWadowski <michalpawelwadowski@gmail.com>
2026-02-18 09:29:00 +0100

    Wall USER

--- proxy.ts
+++ proxy.ts
@@ -1,1 +1,32 @@
 export { auth as proxy } from "./auth";
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+  matcher: ["/((?!_next|favicon.ico).*)"],
+};
+export function middleware(req: NextRequest) {
+  const auth = req.headers.get("authorization");
+
+  const user = process.env.WALL_USER;
+  const pass = process.env.WALL_PASS;
+
+  if (!auth) {
+    return new NextResponse("Auth required", {
+      status: 401,
+      headers: { "WWW-Authenticate": 'Basic realm="Secure"' },
+    });
+  }
+
+  const [, encoded] = auth.split(" ");
+  const decoded = Buffer.from(encoded || "", "base64").toString();
+  const [u, p] = decoded.split(":");
+
+  if (u === user && p === pass) return NextResponse.next();
+
+  return new NextResponse("Unauthorized", {
+    status: 401,
+    headers: { "WWW-Authenticate": 'Basic realm="Secure"' },
+  });
+}
